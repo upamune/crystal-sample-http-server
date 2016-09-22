ef6a5c8087657d10330a289280fbacda631f6416
upamune <jajkeqos@gmail.com>
2016-09-22 23:41:10 +0900

    add simple http server

--- src/crystal-sample-http-server.cr
+++ src/crystal-sample-http-server.cr
@@ -1,5 +1,30 @@
 require "./crystal-sample-http-server/*"
+require "http/server"
+require "option_parser"
 
 module Crystal::Sample::Http::Server
-  # TODO Put your code here
+  port = 8080
+
+  OptionParser.parse! do |parser|
+    parser.on("-p PORT", "--port=PORT", "Specifies the port") { |port_str|
+      port = port_str.to_i if port_str.to_i?
+    }
+  end
+
+  server = HTTP::Server.new("0.0.0.0", port, [
+    HTTP::ErrorHandler.new,
+    HTTP::LogHandler.new,
+  ]) do |context|
+    case context.request.path
+    when "/"
+      context.response.content_type = "text/plain"
+      context.response.status_code = 200
+      context.response.print "Hello, Crystal."
+    else
+      context.response.status_code = 404
+    end
+  end
+
+  puts "Listening on http://0.0.0.0:#{port}"
+  server.listen
 end
