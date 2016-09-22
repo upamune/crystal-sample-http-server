75a091881a851ffa83729114d352c271e376e205
upamune <jajkeqos@gmail.com>
2016-09-22 22:08:28 +0900

    init commit

---
+++ src/crystal-sample-http-server.cr
@@ -0,0 +1,5 @@
+require "./crystal-sample-http-server/*"
+
+module Crystal::Sample::Http::Server
+  # TODO Put your code here
+end
---
+++ src/crystal-sample-http-server/version.cr
@@ -0,0 +1,3 @@
+module Crystal::Sample::Http::Server
+  VERSION = "0.1.0"
+end
